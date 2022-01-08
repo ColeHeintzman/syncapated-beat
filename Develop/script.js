d609e401f04010f8125529f6d67bef179b3862ec
ColeHeintzman <heintzmancoleanthony@gmail.com>
2022-01-08 13:54:18 -0600

    adding input potentials

--- Develop/script.js
+++ Develop/script.js
@@ -9,8 +9,10 @@ var confirmUpper;
 var confirmLower;
 var confirmSpecial;
 var confirmNumber;
-var userChoices;
-
+var upperCase;
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var specialCharacter = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -25,6 +27,7 @@ generateBtn.addEventListener("click", writePassword);
 
 
 function generatePassword() {
+  //*Password Length Question *//
   passwordLength = prompt("How many characters does your password require?  Choose between 8 and 128");
   console.log(" password length " + passwordLength);
     if(passwordLength) {
@@ -33,13 +36,67 @@ function generatePassword() {
     if(passwordLength < 8 || passwordLength > 128){
       alert("The number of characters ( " + passwordLength + " ) is not a valid entry.  Please choose a number between 8 and 128.");
       return generatePassword();
+    }  
+  //*UpperCase Letter Input *//
+  confirmUpper = confirm("Does your password require Upper case letters?  Choose 'ok' for yes, 'cancel' for no.");  
+  if (confirmUpper == true) {
+      alert("Your password will contain Upper Case letters");
     }
-  confirmUpper = confirm("Does your password require Upper case letters?  Choose 'ok' for yes, 'cancel' for no.");
+    else if(confirmUpper == false) {
+      alert("Your password will NOT contain Upper Case letters");
+      }
+  //*Lower case Input*//
   confirmLower = confirm("Does your password require Lower case Letters?  Choose 'ok' for yes, 'cancel' for no.");
+    if (confirmUpper == true) {
+      alert("Your password will contain Lower Case letters");
+      } 
+      else if(confirmUpper == false) {
+      alert("Your password will NOT contain Lower Case letters");
+      }
+  //* Special Character Input*//
   confirmSpecial = confirm("Does your password require Special Characters?  Choose 'ok' for yes, 'cancel' for no.");
+    if (confirmUpper == true) {
+      alert("Your password will contain Special Characters");
+      }
+      else if(confirmUpper == false) {
+      alert("Your password will NOT contain Special Characters");
+      }
+  //* Number Input *//
   confirmNumber = confirm("Does your password require Numbers?  Choose 'ok' for yes, 'cancel' for no.");
-  console.log(confirmUpper, confirmLower, confirmSpecial, confirmNumber);
-
-    
-
+    if (confirmUpper == true) {
+      alert("Your password will contain Numbers");
+      }
+      else if(confirmUpper == false) {
+      alert("Your password will NOT contain Numbers");
+      }
+      console.log(confirmUpper, confirmLower, confirmSpecial, confirmNumber);
+  //*Must Choose One Option if all False*//
+      if(confirmUpper == false && confirmLower == false && confirmSpecial == false && confirmNumber == false) {
+      userChoices = alert("You must choose atleast one of the previous options.");
+      }
+  //*Potential Input Selections*//
+      if(confirmUpper == true && confirmLower == false && confirmSpecial == false && confirmNumber == false) {
+        userChoices = lowerCase;
+        }
+      if(confirmUpper == false && confirmLower == true && confirmSpecial == false && confirmNumber == false) {
+        userChoices = upperCase;
+        }
+      if(confirmUpper == false && confirmLower == false && confirmSpecial == true && confirmNumber == false) {
+       userChoices = specialCharacter;
+        }
+      if(confirmUpper == false && confirmLower == false && confirmSpecial == false && confirmNumber == true) {
+        userChoices = numbers;
+        }
+      if(confirmUpper == true && confirmLower == true && confirmSpecial == true && confirmNumber == false) {
+        userChoices = lowerCase;
+        }
+      if(confirmUpper == false && confirmLower == true && confirmSpecial == true && confirmNumber == true) {
+        userChoices = upperCase;
+        }
+      if(confirmUpper == true && confirmLower == false && confirmSpecial == true && confirmNumber == true) {
+        userChoices = specialCharacter;
+        }
+      if(confirmUpper == true && confirmLower == true && confirmSpecial == false && confirmNumber == true) {
+        userChoices = numbers;
+        }
 }
